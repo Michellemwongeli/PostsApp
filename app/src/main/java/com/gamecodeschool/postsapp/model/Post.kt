4bc10c45dcf26463716360592dcc52a0df681353
Michellemwongeli <nthumomichelle@gmail.com>
2020-09-11 15:50:12 +0300

    first commit

---
+++ app/src/main/java/com/gamecodeschool/postsapp/model/Post.kt
@@ -0,0 +1,5 @@
+package com.gamecodeschool.postsapp.model
+
+import android.icu.text.CaseMap
+
+data class Post(var userId:Int, var id:Int, var title:String, var body:String)
---
+++ app/src/main/java/com/gamecodeschool/postsapp/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.gamecodeschool.postsapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.gamecodeschool.postsapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
